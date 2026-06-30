b2309be3b3aca6cde8f23516e0618bb9545a571b
c5zr5kb2ys-collab <c5zr5kb2ys@privaterelay.appleid.com>
2026-06-30 23:06:02 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -37,3 +37,36 @@ function showLetters() {
 function createLetter(text) {
   return `<div class="letter">${text}</div>`;
 }
+function createLetter(text) {
+  return `<div class="letter" onclick="selectLetter('${text}')">${text}</div>`;
+}
+
+function selectLetter(theme) {
+  scene.innerHTML = `
+    <h1 class="title">Who is it for?</h1>
+    <p class="subtitle">${theme}</p>
+
+    <div class="recipients">
+      <div class="recipient" onclick="openLetter('${theme}', 'ひろと')">ひろと</div>
+      <div class="recipient" onclick="openLetter('${theme}', 'ゆり')">ゆり</div>
+    </div>
+  `;
+}
+
+function openLetter(theme, name) {
+  scene.innerHTML = `
+    <h1 class="title">${theme}</h1>
+    <p class="subtitle">To ${name}</p>
+
+    <div class="paper">
+      <p class="message">
+        ここにメッセージが入ります。<br><br>
+        この瞬間のために書かれた手紙です。
+      </p>
+
+      <p class="from">From ○○</p>
+
+      <button class="back" onclick="location.reload()">Back</button>
+    </div>
+  `;
+}
